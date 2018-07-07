279c42a39c2187fedbbb60b7e2e7ca0ac40e111c
Ryan <RyanMcKeown82@Gmail.com>
2018-07-07 16:29:42 +0800

    create initial

---
+++ main.rb
@@ -0,0 +1,45 @@
+### Comments ###
+# ---------------
+
+# Single-line comment
+
+=begin
+Multi-line
+com
+ment
+=end
+
+### Mathematical operators ###
+# -----------------------------
+
+# Addition (+)
+# Subtraction (-)
+# Multiplication (*)
+# Exponentiation (**)
+# Division (/)
+# Getting a remainder (modulo) (%)
+
+### I/O ###
+# ----------
+
+puts "Hello, world!" 
+# puts is short for "put string"
+
+print "Hello, world!"
+
+# gets
+# `gets` is used for when you need to get user input
+
+# chomp 
+# `chomp` Ruby automatically adds a newline after `gets`, to remove that you use chomp
+
+### Variables ###
+# ----------------
+
+# my_variable = value
+my_string = "Ryan"
+my_int  = 13
+my_bool = true
+# can be false
+
+
